fe195d6a95798dde13acc676eac3c948480b8bc0
Igor Ermakov <igor.ermakov@opensoftdev.ru>
2016-06-08 13:42:28 +0300

    Remove 'alias' property from settings

--- README.md
+++ README.md
@@ -10,21 +10,19 @@ Config changes are applied only at service restart. If service was installed usi
 [server]
 # server port
 port=8090
-# printer group names via |
+# printer group names via | and also alias through which the printer is available by "printer" parameter in the URL
 printers=zebra_ups|hp_net
 # alias of default printer if "printer" parameter is not specified in the URL
 default_printer=z_ups
 
 [zebra_ups]
-# alias through which the printer is available by "printer" parameter in the URL
-alias=z_ups
 # printer name for lpq, empty for default
 name=Zebra_UPS
 # printer host, empty for local
 host=
 # printer can print EPL
 accepts_raw=true
-# printer ccan print files
+# printer can print files
 accepts_files=false
 
 [hp_net]
--- main.cpp
+++ main.cpp
@@ -17,21 +17,15 @@ int main(int argc, char *argv[])
     int serverPort = serverGroup->value("port", 8090, Proof::Settings::NotFoundPolicy::Add).toInt();
     QStringList printerSections = serverGroup->value("printers", "", Proof::Settings::NotFoundPolicy::Add)
             .toString().split('|', QString::SkipEmptyParts);
-    QString defaultPrinterSection = serverGroup->value("default_printer", QString(), Proof::Settings::NotFoundPolicy::Add).toString();
+    QString defaultPrinter = serverGroup->value("default_printer", "", Proof::Settings::NotFoundPolicy::Add).toString();
     QList<PrinterInfo> printerInfos;
-    QString defaultPrinter;
     for (const QString &printerSection : printerSections) {
         Proof::SettingsGroup *printerGroup = a.settings()->group(printerSection.trimmed(), Proof::Settings::NotFoundPolicy::Add);
-        QString alias = printerGroup->value("alias", "", Proof::Settings::NotFoundPolicy::Add).toString();
         QString name = printerGroup->value("name", "", Proof::Settings::NotFoundPolicy::Add).toString();
         QString host = printerGroup->value("host", "", Proof::Settings::NotFoundPolicy::Add).toString();
         bool acceptsRaw = printerGroup->value("accepts_raw", false, Proof::Settings::NotFoundPolicy::Add).toBool();
         bool acceptsFiles = printerGroup->value("accepts_files", false, Proof::Settings::NotFoundPolicy::Add).toBool();
-        if (!alias.isEmpty()) {
-            printerInfos << PrinterInfo{alias, name, host, acceptsRaw, acceptsFiles};
-            if (defaultPrinterSection == printerGroup->name())
-                defaultPrinter = alias;
-        }
+        printerInfos << PrinterInfo{printerGroup->name(), name, host, acceptsRaw, acceptsFiles};
     }
 
     if (printerInfos.isEmpty()) {
